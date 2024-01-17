d737628983b481a241d10f2fdb449599e83a22b4
PankajPratihar <143259948+PankajPratihar@users.noreply.github.com>
2024-01-17 11:08:50 +0530

    first commit

--- java.js
+++ java.js
@@ -3,7 +3,7 @@
 const body=document.querySelector('body');
 const button=document.querySelector('button');
 const colors=['black','red','green','pink','violet','yellow'];
-body.style.backgroundColor='violet';
+body.style.backgroundColor='black';
 button.addEventListener('click',function(){
     const colorIndex=parseInt(Math.random()*colors.length);
     body.style.backgroundColor=colors[colorIndex]
